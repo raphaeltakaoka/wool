f401a9b5180fe0e04890a13cb41d38abb256d836
Raphael Takaoka <raphaeltakaoka@Raphaels-MacBook-Pro.local>
2017-05-17 16:53:22 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var modal = document.querySelector(".modal");
+var opacity = 0;
+var width = 50;
+
+function mostrarProduto1() {
+  opacity++;
+
+  console.log(opacity);
+  modal.style.opacity = opacity;
+  if (opacity > 1) {
+    opacity = 0;
+    modal.style.opacity = opacity;
+
+  }
+}
